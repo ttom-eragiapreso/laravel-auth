8d3c8905e3722929c4aafaa07cc3e77f60f458ff
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2023-01-17 15:44:38 +0100

    aggiunto messaggio nella index di dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+          <div class="card-body">
+            @if (session('status'))
+              <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+              </div>
+            @endif
+            <p>You are logged in, {{ Auth::user()->name }}</p>
+            <p>Your email is: {{ Auth::user()->email }}</p>
+            <p>Your encrypted password is: {{ Auth::user()->password }}</p>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
